0b38d4255911875e393e927191f4204d2083ad3c
trambz <trambz@local>
2022-10-23 20:26:26 +0200

    Getting life back

--- src/engine/hud/Hud.ts
+++ src/engine/hud/Hud.ts
@@ -10,8 +10,8 @@ export class Hud{
     public update(){
     }
     public addGameEntity(){
-      console.log("sending game entity", this._engineService)
       this._engineService.send("ADD_GAME_ENTITY")
+      this.refresh()
     }
     public init(){
         this._engineService = machine;
@@ -20,32 +20,35 @@ export class Hud{
         document.getElementById("hud").appendChild(addItem);
         addItem.addEventListener("click", () => { this.addGameEntity()});
 
-        this._engineEntities.forEach(entity =>{ 
+        
+    }
+    private refresh(){
+      this._engineEntities.forEach(entity =>{ 
           
-          const domEntity = document.getElementById(entity.uuid);
-          
-          if (!domEntity){
-            const section  = document.createElement("section");
-            section.id = entity.uuid;
-            section.innerHTML = entity.uuid;
-            document.getElementById("hud").appendChild(section)
-            entity.components.forEach(entry => {
-              const section  = document.createElement("article");
-              section.id = entry.uuid;
-              const entries = Object.keys(entry)
-                                .filter(key => key !== "uuid")
-                                .map(key => `${key.split("_")[1]} ${entry[key]}`);
-                                
-              entries.forEach(entry => {
-                const d = document.createElement('div')
-                d.innerHTML = entry;
-                section.appendChild(d);
-              })
-              document.getElementById("hud").appendChild(section)
+        const domEntity = document.getElementById(entity.uuid);
+        
+        if (!domEntity){
+          const section  = document.createElement("section");
+          section.id = entity.uuid;
+          section.innerHTML = entity.uuid;
+          document.getElementById("hud").appendChild(section)
+          entity.components.forEach(entry => {
+            const section  = document.createElement("article");
+            section.id = entry.uuid;
+            const entries = Object.keys(entry)
+                              .filter(key => key !== "uuid")
+                              .map(key => `${key.split("_")[1]} ${entry[key]}`);
+                              
+            entries.forEach(entry => {
+              const d = document.createElement('div')
+              d.innerHTML = entry;
+              section.appendChild(d);
             })
-    
-          }
-         
-        })
-      }
+            document.getElementById("hud").appendChild(section)
+          })
+  
+        }
+       
+      })
+    }
 }
